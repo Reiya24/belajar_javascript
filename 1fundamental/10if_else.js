203b19edb8fdc94940de78ba639da848f5ebce43
Reiya Tenggara <reiya2307@gmail.com>
2023-05-18 23:22:05 +0700

    fsda

--- 1fundamental/10if_else.js
+++ 1fundamental/10if_else.js
@@ -9,6 +9,35 @@ console.log("========")
 
 const nilai = 75;
 
-if (nilai > 75) {
-    console.log("lulus")
+if (nilai >= 75) {
+    console.log("lulus");
+} else {
+    console.log("tidak lulus")
+}
+
+console.log("========")
+
+const language = "english";
+
+if (language === "english") {
+    console.log("hello world")
+} else if (language === "indonesia") {
+    console.log("hallo dunia")
+} else {
+    console.log("language not suported")
+}
+
+// ternary operator
+console.log(true ? "benar": "salah")
+
+// truthy falsy in javascript
+// Number 0
+// BigInt 0n
+// String kosong seperti “” atau ‘’
+// null
+// undefined
+// NaN, atau Not a Number
+
+if (0){
+    console.log("print me")
 }
